ea464c7cbbf7fdb3175ea0f48d7ac66730eeb20c
leonardo.girotto <leonardo.bueno@bis2bis.com.br>
2024-06-13 21:40:06 -0300

    🎨 Atualizando estrutura de pastas

---
+++ master/MEDConsult/src/main/java/Model/Medico.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+import Model.Usuario;
+
+/**
+ *
+ * @author mathe
+ */
+public class Medico {
+    private String crm;
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+}
---
+++ master/MEDConsult/src/main/java/Model/Paciente.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+import Model.Usuario;
+
+/**
+ *
+ * @author mathe
+ */
+public class Paciente {
+    private String cpf;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+}
